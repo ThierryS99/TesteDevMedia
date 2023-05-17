8e4e9aba7327166c33978f6eb51b4a685234707e
Thierry Stuhler <thierrystuhler@gmail.com>
2023-05-16 21:08:41 -0300

    Submissao inicial.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TesteDevMedia.Models;
+
+namespace TesteDevMedia.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public PostController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult PostPadraoView()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/PostModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TesteDevMedia.Models
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Categoria { get; set; }
+    }
+}
