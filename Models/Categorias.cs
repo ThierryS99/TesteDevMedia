f119d129f65d328c6d7daa4285742383fa1fc8fa
Thierry Stuhler <thierrystuhler@gmail.com>
2023-05-17 21:55:36 -0300

    Atualizando projeto.

---
+++ Models/Categorias.cs
@@ -0,0 +1,13 @@
+﻿namespace TesteDevMedia.Models
+{
+    public class Categorias
+    {
+        private string InteligenciaArtificial;
+        private string CSharp;
+        private string Python;
+        private string Html;
+        private string JavaScript;
+        private string Java;
+        private string Sql;
+    }
+}
